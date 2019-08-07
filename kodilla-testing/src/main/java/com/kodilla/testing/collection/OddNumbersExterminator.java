b942f24f561dcd946c630803d93ab408ad1f292a
venustuspl <venustus.pl@gmail.com>
2019-08-07 21:56:24 +0200

    Foreach replaced Iterator

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,22 +1,19 @@
 package com.kodilla.testing.collection;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 
 public class OddNumbersExterminator {
-//  List<Integer> intList = new ArrayList<Integer>();
 
 
     public static List<Integer> exterminate(ArrayList<Integer> numbers) {
+
         List<Integer> evenList = new ArrayList<Integer>();
-        Iterator<Integer> numbersIterator = numbers.iterator();
 
-
-        while (numbersIterator.hasNext()) {
-            int a = numbersIterator.next();
-            if (a % 2 == 0) {
-                evenList.add(a);
+        for (Integer entry : numbers) {
+            if (entry % 2 == 0) {
+                evenList.add(entry);
             }
         }
 
