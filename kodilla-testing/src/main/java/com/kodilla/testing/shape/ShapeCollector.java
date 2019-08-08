5f50950dd7dd56a92bb5fe7bea45cbc0eb784380
venustuspl <venustus.pl@gmail.com>
2019-08-08 19:29:47 +0200

    ShapeTests added1

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,4 +1,81 @@
 package com.kodilla.testing.shape;
 
-public class ShapeCollector {
+import java.util.List;
+import java.util.ArrayList;
+
+interface Shape {
+    public String getShapeName();
+
+    public double getField();
+
+}
+
+class ShapeCollector {
+    List<Shape> figureList = new ArrayList<Shape>();
+
+    public void addFigure(Shape shape) {
+    }
+
+    ;
+
+    public void removeFigure(Shape shape) {
+
+    }
+
+    ;
+
+    public Shape getFigure(int i) {
+        Shape shape = new Square();
+        return shape;
+    }
+
+    ;
+
+    public void showFigures() {
+
+    }
+
+    ;
+}
+
+class Square implements Shape {
+    public String getShapeName() {
+        return "";
+    }
+
+    ;
+
+    public double getField() {
+        return 1.0;
+    }
+
+    ;
+}
+
+class Triangle implements Shape {
+    public String getShapeName() {
+        return "";
+    }
+
+    ;
+
+    public double getField() {
+        return 1.0;
+    }
+
+    ;
+}
+
+class Circle implements Shape {
+    public String getShapeName() {
+        return "";
+    }
+
+    ;
+
+    public double getField() {
+        return 1.0;
+    }
+
+    ;
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -1,4 +1,70 @@
 package com.kodilla.testing.shape;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class ShapeCollectorTestSuite {
+    @Test
+    public void testAddShapeSquare() {
+
+        //Given
+        Shape shape = new Square("kwadrat", 5);
+
+        //When
+        Boolean result;
+        result = ShapeCollector.addFigure(shape);
+
+        //Then
+        assertTrue(result);
+    }
+
+}
+
+
+    @Test
+    public void testRemoveShape() {
+        List<Shape> figureList = new ArrayList<Shape>();
+
+        Shape shape = new Triangle("trójkąt1", 5, 2);
+        figureList.add(shape);
+
+        removeFigure(shape);
+
+
+        Assert.assertFalse(figureList.contains(shape));
+    }
+
+    @Test
+    public void testGetFigure() {
+        List<Shape> figureList = new ArrayList<Shape>();
+
+        Shape shape = new Triangle("trójkąt2", 5, 2);
+        figureList.add(shape);
+
+
+        assertEquals(0, figureList.indexOf(shape));
+    }
+
+    @Test
+    public void testShowFigures() {
+        List<Shape> figureList = new ArrayList<Shape>();
+
+        Shape shape = new Triangle("trójkąt2", 5, 2);
+        Shape shape1 = new Square("kwadrat", 2);
+        Shape shape2 = new Circle("koło", 5);
+        figureList.add(shape);
+        figureList.add(shape1);
+        figureList.add(shape2);
+
+
+        assertEquals(3, figureList.size() + 1);
+    }
+
+
 }
