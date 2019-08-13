34a44669c27210262b87254454024819dbb749dd
venustuspl <venustus.pl@gmail.com>
2019-08-13 22:46:37 +0200

    7.2.0.1 - class stream.immutable.ForumUser added

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.immutable;
+
+public class ForumUser {
+    private final String username;
+    private final String realname;
+
+    public ForumUser(final String username, final String realname) {
+        this.username = username;
+        this.realname = realname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+}
