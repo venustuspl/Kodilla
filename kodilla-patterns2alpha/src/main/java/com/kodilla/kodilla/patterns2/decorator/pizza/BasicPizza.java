c25b0f776794587dcca2294f03097a17f0a776c0
venustuspl <venustus.pl@gmail.com>
2019-11-11 13:48:28 +0100

    20.2

---
+++ kodilla-patterns2alpha/src/main/java/com/kodilla/kodilla/patterns2/decorator/pizza/BasicPizza.java
@@ -0,0 +1,15 @@
+package com.kodilla.kodilla.patterns2.decorator.pizza;
+
+import java.math.BigDecimal;
+
+public class BasicPizza implements PizzaOrder {
+    @Override
+    public BigDecimal getCost() {
+        return new BigDecimal(15);
+    }
+
+    @Override
+    public String getComponents() {
+        return "Pizza z: sos pomidorowy, ser";
+    }
+}
---
+++ kodilla-patterns2alpha/src/main/java/com/kodilla/kodilla/patterns2/decorator/pizza/HamOrderDecorator.java
@@ -0,0 +1,19 @@
+package com.kodilla.kodilla.patterns2.decorator.pizza;
+
+import java.math.BigDecimal;
+
+public class HamOrderDecorator extends AbstractPizzaOrderDecorator {
+    protected HamOrderDecorator(PizzaOrder basicPizza) {
+        super(basicPizza);
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        return super.getCost().add(new BigDecimal(10));
+    }
+
+    @Override
+    public String getComponents() {
+        return super.getComponents() + ", szynka";
+    }
+}
